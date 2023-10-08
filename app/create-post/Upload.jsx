7e33763659874e5895801cb912cadb7abc6e1411
usaame19 <usaameshsaid19@gmail.com>
2023-10-08 20:20:57 +0300

    updated

--- app/create-post/Upload.jsx
+++ app/create-post/Upload.jsx
@@ -3,91 +3,94 @@ import React, { useState } from 'react';
 import { CldUploadWidget } from 'next-cloudinary';
 import { useRouter } from 'next/navigation';
 import { getBaseUrl } from '../util/baseURL';
+
+export const dynamic = 'auto';
 const Upload = () => {
-  const [imageUrl, setImageUrl] = useState(null);
-  const [content, setContent] = useState('');
-  const [title, setTitle] = useState('');
-  const router = useRouter();
 
-  const handleSubmit = async (e) => {
-    const baseURL = getBaseUrl()
-    e.preventDefault();
+    const baseURL = getBaseUrl();
 
-    const registerPost = await fetch(`${baseURL}/api/posts`, {
-      method: 'POST',
-      body: JSON.stringify({ title, content, url: imageUrl }),
-    });
+    const [imageUrl, setImageUrl] = useState(null);
+    const [content, setContent] = useState(null);
+    const [title, setTitle] = useState(null);
+    const router = useRouter();
 
-    setContent('');
-    setTitle('');
-    setImageUrl(null);
+    const handleSubmit = async (e) => {
 
-    router.push('/');
-  };
+        e.preventDefault();
 
-  return (
-    <div className="h-screen flex justify-center items-center">
-      <div className="w-full max-w-md p-4">
-        <h1 className="text-2xl font-bold mb-4">Create a New Post</h1>
-        <form onSubmit={handleSubmit} className="bg-white p-6 rounded-md shadow-md">
-          <div className="mb-4">
-            <label htmlFor="title" className="block text-sm text-gray-600 mb-2">
-              Title:
-            </label>
-            <input
-              type="text"
-              id="title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              className="w-full px-3 py-2 border rounded-md outline-none focus:border-indigo-500 focus:ring"
-              required
-            />
-          </div>
 
-          <div className="mb-4">
-            <label htmlFor="content" className="block text-sm text-gray-600 mb-2">
-              Content:
-            </label>
-            <textarea
-              id="content"
-              value={content}
-              onChange={(e) => setContent(e.target.value)}
-              className="w-full px-3 py-2 border rounded-md outline-none focus:border-indigo-500 focus:ring"
-              required
-              rows="4"
-            ></textarea>
-          </div>
+        const registerPost = await fetch(`${baseURL}/api/post`, {
+            method: 'POST',
+            body: JSON.stringify({ title, content, url: imageUrl })
+        });
 
-          <div className="mb-4">
-            <label className="block text-sm text-gray-600 mb-2">Image Preview:</label>
-            {imageUrl && (
-              <img src={imageUrl} alt="Preview" className="w-full h-40 object-cover rounded-md" />
-            )}
-          </div>
+        setContent("");
+        setTitle("");
+        setImageUrl(null);
 
-          <CldUploadWidget uploadPreset="snjuchgw" onUpload={(result, widget) => setImageUrl(result.info.url)}>
-            {({ open }) => (
-              <button
-                className="w-full bg-indigo-700 py-2 px-4 rounded-md text-white"
-                onClick={(e) => {
-                  e.preventDefault();
-                  open();
-                }}
-              >
-                Upload an Image
-              </button>
-            )}
-          </CldUploadWidget>
+        router.push('/');
 
-          {imageUrl && (
-            <button type="submit" className="w-full bg-indigo-700 py-2 px-4 rounded-md text-white mt-4">
-              Create the Post
-            </button>
-          )}
-        </form>
-      </div>
-    </div>
-  );
+
+    };
+
+
+    return (
+        <div className='h-auto mx-auto  flex justify-center items-center max-w-4xl mt-24'>
+
+            <div className='flex flex-col space-y-6'>
+                <form onSubmit={handleSubmit} className="bg-white p-6 max-w-sm mx-auto rounded-md shadow-md">
+                    <div className="mb-4">
+                        <label htmlFor="title" className="block text-sm text-gray-600 mb-2">Title:</label>
+                        <input
+                            type="text"
+                            id="title"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            className="w-full px-3 py-2 border rounded-md outline-none focus:border-indigo-500 focus:ring"
+                            required
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="content" className="block text-sm text-gray-600 mb-2">Content:</label>
+                        <textarea
+                            id="content"
+                            value={content}
+                            onChange={(e) => setContent(e.target.value)}
+                            className="w-full px-3 py-2 border rounded-md outline-none focus:border-indigo-500 focus:ring"
+                            required
+                        ></textarea>
+                    </div>
+
+                    <CldUploadWidget uploadPreset="lunuwzor"
+                        onUpload={(result, widget) =>
+                            // @ts-ignore
+                            setImageUrl(result.info.url)}
+                    >
+                        {({ open }) => {
+
+                            return (
+                                <button className="bg-indigo-700 py-4 px-2 rounded-md m-4 text-white" onClick={(e) => { e.preventDefault(); open(); }}>
+                                    Upload an Image
+                                </button>
+                            );
+                        }}
+                    </CldUploadWidget>
+                    {imageUrl && (
+                        <div className="mb-4">
+                            <label className="block text-sm text-gray-600 mb-2">Image Preview:</label>
+                            <img src={imageUrl} alt="Preview" className="w-full h-64 object-cover rounded-md" />
+                        </div>
+                    )}
+
+                    {imageUrl && (<button type="submit" className="w-full bg-indigo-700 py-2 px-4 rounded-md text-white">
+                        Create the post
+                    </button>)}
+                </form>
+
+            </div>
+        </div>
+    );
 };
 
 export default Upload;
